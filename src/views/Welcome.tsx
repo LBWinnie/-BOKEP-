e8d29070cd9adf602560b603fca8114d338ccf69
LBWinnie <git@github.com:LBWinnie>
2023-03-29 00:26:10 +0800

    feature:welcome/1

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,9 +1,15 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/sakura-festival.svg'
 export const Welcome = defineComponent({
   setup: (props, context) => {
-    return () => (
-      <div><RouterView/></div>
-    )
+    return () => <div class={s.wrapper}>
+      <header>
+        <img src={logo} />
+        <h1>+BOKEP+</h1>
+      </header>
+      <main class={s.main}><RouterView /></main>
+    </div>
   }
 })
