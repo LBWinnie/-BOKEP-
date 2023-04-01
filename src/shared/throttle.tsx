9c0c890906cd93cc8a40a4934043e713b3783674
LBWinnie <git@github.com:LBWinnie>
2023-04-01 23:11:18 +0800

    避免使用any类型之TS体操

--- src/shared/throttle.tsx
+++ src/shared/throttle.tsx
@@ -1,13 +1,15 @@
-export const throttle = (fn: Function, time: number) => {
-    let timer: number | undefined = undefined
-    return (...args: any[]) => {
-      if (timer) {
-        return
-      } else {
-        fn(...args)
-        timer = setTimeout(() => {
-          timer = undefined
-        }, time)
-      }
+export const throttle = <T extends ((...args: unknown[]) => any)>(fn: T, time: number) => {
+  let timer: number | undefined = undefined
+  let result: ReturnType<T>
+  return (...args: Parameters<T>) => {
+    if (timer) {
+      return result
+    } else {
+      result = fn(...args)
+      timer = setTimeout(() => {
+        timer = undefined
+      }, time)
+      return result
     }
   }
+}
