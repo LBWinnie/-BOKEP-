bbeaaabdd7152b86d895d7983c1bbdcc68301622
LBWinnie <git@github.com:LBWinnie>
2023-04-12 23:15:46 +0800

    每次切换tab时重新渲染图表

--- src/components/statistics/LineChart.tsx
+++ src/components/statistics/LineChart.tsx
@@ -1,36 +1,38 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
-import s from './LineChart.module.scss';
-import * as echarts from 'echarts';
-export const LineChart = defineComponent({
-  setup: (props, context) => {
-    const refDiv = ref<HTMLDivElement>()
-    onMounted(() => {
-      if (refDiv.value === undefined) { return }
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(refDiv.value);
-      // 绘制图表
-      myChart.setOption({
-        grid: [
-          { left: 0, top: 0, right: 0, bottom: 20 }
-        ],
-        xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-          type: 'value'
-        },
-        series: [
-          {
-            data: [150, 230, 224, 218, 135, 147, 260],
-            type: 'line'
-          }
-        ]
-      });
+import { defineComponent, PropType, ref } from "vue";
+import { FormItem } from "../../shared/Form";
+import s from "./Charts.module.scss";
+import { LineChart } from "./LineChart";
+import { PieChart } from "./PieChart";
+import { Bars } from "./Bars";
 
-    })
+export const Charts = defineComponent({
+  props: {
+    startDate: {
+      type: String as PropType<string>,
+      required: false,
+    },
+    endDate: {
+      type: String as PropType<string>,
+      required: false,
+    },
+  },
+  setup: (props, context) => {
+    const category = ref("expenses");
     return () => (
-      <div ref={refDiv} class={s.wrapper}></div>
-    )
-  }
-})
+      <div class={s.wrapper}>
+        <FormItem
+          label="类型"
+          type="select"
+          options={[
+            { value: "expenses", text: "支出" },
+            { value: "income", text: "收入" },
+          ]}
+          v-model={category.value}
+        />
+        <LineChart />
+        <PieChart />
+        <Bars />
+      </div>
+    );
+  },
+});
