d59c69e6b5a50de7830dfa8577a8d6739be62dca
LBWinnie <git@github.com:LBWinnie>
2023-04-14 20:14:21 +0800

    合并 /start 和 /items

--- src/shared/SkipFeatures.tsx
+++ src/shared/SkipFeatures.tsx
@@ -1,15 +1,15 @@
-import { defineComponent, PropType } from 'vue';
-import { RouteLocationRaw, RouterLink } from 'vue-router';
-import s from './SkipFeatures.module.scss';
+import { defineComponent, PropType } from "vue";
+import { RouteLocationRaw, RouterLink } from "vue-router";
+import s from "./SkipFeatures.module.scss";
 export const SkipFeatures = defineComponent({
   setup: (props, context) => {
     const onClick = () => {
-      localStorage.setItem('skipFeatures', 'yes')
-    }
+      localStorage.setItem("skipFeatures", "yes");
+    };
     return () => (
       <span onClick={onClick}>
-        <RouterLink to="/start">跳过</RouterLink>
+        <RouterLink to="/items">跳过</RouterLink>
       </span>
-    )
-  }
-})
+    );
+  },
+});
