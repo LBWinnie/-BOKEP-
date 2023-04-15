7aad79ea876825f714b0b8d3d73720f4b4098dce
LBWinnie <git@github.com:LBWinnie>
2023-04-15 20:17:51 +0800

    更改为支持一种删除方式

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -45,17 +45,10 @@ export const TagEdit = defineComponent({
               <div class={s.actions}>
                 <Button
                   level="danger"
-                  class={s.removeTags}
-                  onClick={() => onDelete()}
-                >
-                  删除标签
-                </Button>
-                <Button
-                  level="danger"
                   class={s.removeTagsAndItems}
                   onClick={() => onDelete({ withItems: true })}
                 >
-                  删除标签和记账
+                  删除标签及对应记账
                 </Button>
               </div>
             </>
